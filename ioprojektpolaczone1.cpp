434dca022b921d176df0e9e2411bf322945fe531
Sowa <96268828+Sowa00@users.noreply.github.com>
2022-01-13 22:27:55 +0100

    Update ioprojektpolaczone1.cpp

--- ioprojektpolaczone1.cpp
+++ ioprojektpolaczone1.cpp
@@ -13,25 +13,25 @@ HANDLE kolor; //zmienna przetrzymująca nasze kolorki (tak działa w windowsie)
 using namespace std;
 
 struct node {
-    string val;
-    node* next;
+	string val;
+	node* next;
 };
 
 void AddToList(node*& H, string x) {
-    node* p = new node;
-    p->val = x;
-    p->next = H;
-    H = p;
+	node* p = new node;
+	p->val = x;
+	p->next = H;
+	H = p;
 }
 
 void Show(node* H) {
-    cout << "H -> ";
-    node* p = H;
-    while (p != NULL) {
-        cout << p->val << " -> ";
-        p = p->next;
-    }
-    cout << "NULL" << endl;
+	cout << "H -> ";
+	node* p = H;
+	while (p != NULL) {
+		cout << p->val << " -> ";
+		p = p->next;
+	}
+	cout << "NULL" << endl;
 }
 
 class Dane_kontaktowe
@@ -45,10 +45,10 @@ public:
     string kodpocztowy = "00-000";
     string miasto = "Krakow";
     string kraj = "Polska";
-    friend void menu(int stan_uz);
     void wyswietl_kontakt()
     {
         SetConsoleTextAttribute(kolor, 15);
+        cout << "Cena wypożyczenia każdego filmu wynosi 10zl" << endl;
         cout << "Imie: " << imie << endl << "Nazwisko: " << nazwisko << endl;
         cout << "Mail: " << mail << endl << "Nr telefonu: " << numer_tel << endl;
         cout << "Ulica: " << ulica << endl << "Miasto: " << miasto << endl;
@@ -92,10 +92,12 @@ public:
         cin >> miasto;
         cout << endl << "Kraj: ";
         cin >> kraj;
-        if (Weryfikacja_danych(listaloginow, login) == 0)
+        if (Weryfikacja_konta(listaloginow, login) == 0)
         {
+            Show(listaloginow);
             AddToList(listaloginow, login);
             cout << "Zarejestrowano" << endl;
+            Show(listaloginow);
         }
         else
         {
@@ -105,29 +107,26 @@ public:
         }
         return 0;
     }
-
+    int Zalogowano(int stan)
+    {
+        return 1;
+    }
 
     int Logowanie() //funkcja logujaca
     {
         string l, h;
         do
         {
+            SetConsoleTextAttribute(kolor, 15);
             SetConsoleTextAttribute(kolor, 8);
             cout << endl << "Login: ";
-            SetConsoleTextAttribute(kolor, 15);
             cin >> l;
-            SetConsoleTextAttribute(kolor, 8);
             cout << endl << "Haslo: ";
-            SetConsoleTextAttribute(kolor, 15);
             cin >> h;
             if (l == login && h == haslo)
             {
-                system("cls");
                 cout << endl << "Zalogowano" << endl;
-                SetConsoleTextAttribute(kolor, 8);
-                system("pause");
-                stan_uz = 1;
-                return stan_uz;
+                stan_uz = Zalogowano(stan_uz);
             }
             else
             {
@@ -145,9 +144,7 @@ public:
                 {
                     SetConsoleTextAttribute(kolor, 10);
                     cout << "Anulowano logowanie" << endl;
-                    SetConsoleTextAttribute(kolor, 8);
-                    stan_uz = false;
-                    break;
+                    // tu przejscie do menu
                 }
             }
         } while (stan_uz == 0);
@@ -178,25 +175,22 @@ public:
                 SetConsoleTextAttribute(kolor, 10);
                 cout << endl << "Haslo zostalo zmienione" << endl;
                 haslo = newhaslo;
-                stan_uz = false;
             }
             else
             {
                 SetConsoleTextAttribute(kolor, 10);
                 cout << endl << "Hasla nie sa sobie rowne" << endl;
-                stan_uz = false;
             }
         }
         else
         {
             SetConsoleTextAttribute(kolor, 10);
             cout << "Stare haslo nieprawidlowe" << endl;
-            stan_uz = false;
         }
         return 0;
     }
 
-    int Weryfikacja_danych(node*& listaloginow, string login) //funkcja sprawdzajaca czy dane z rejestracji nie powtarzaja sie z innymi uzytkownikami
+    int Weryfikacja_konta(node*& listaloginow, string login) //funkcja sprawdzajaca czy dane z rejestracji nie powtarzaja sie z innymi uzytkownikami
     {
         if (listaloginow == NULL)
         {
@@ -226,136 +220,118 @@ Dane_kontaktowe Dane_kontaktowe;
 node* listaloginow = NULL;
 
 void menu(int stan_uz) {
-    bool stan = true; //wykorzystywane do pętli for
-    do {
-        system("cls");
-        stan = true;
+	bool stan = true; //wykorzystywane do pętli for
+	do {
+		stan = true;
+		SetConsoleTextAttribute(kolor, 10); //kolor komunikatów systemu
+		cout << "Testowa wersja menu (wiele moze jeszcze ulec zmianie)\n" << endl;
+		SetConsoleTextAttribute(kolor, 15); //domyślny kolor
+		cout << "Prosze wybrac odpowiednia opcje za pomoca litery podanej w nawiasie kwadratowym: " << endl;
+		cout << "\n[L]  Logowanie;\n[R]  Rejestracja;\n[Z]  Zmiana hasla;\n[W]  Wyszukaj film;\n[K]  Kategorie filmów;\n[P]  Proponowane filmy;\n[U]  Lista życzeń;\n[D]  Dane kontaktowe;" << endl;
+		char wybor;
+		SetConsoleTextAttribute(kolor, 8);
+		cout << "\n\nTwój wybór: ";
+		SetConsoleTextAttribute(kolor, 15);
+		cin >> wybor;
+		if (stan_uz == 1) { //Zalogowany uzytkownik
+			switch (toupper(wybor)) {
+				SetConsoleTextAttribute(kolor, 15);
 
-        cout << "Stan uzytkownika: " << stan_uz << "\n\n";
+				//===================UWAGA! Funckie nie przyjmują zmiennych!===================//
 
-        SetConsoleTextAttribute(kolor, 10); //kolor komunikatów systemu
-        cout << "Testowa wersja menu (wiele moze jeszcze ulec zmianie)\n" << endl;
-        SetConsoleTextAttribute(kolor, 15); //domyślny kolor
-        cout << "Prosze wybrac odpowiednia opcje za pomoca litery podanej w nawiasie kwadratowym: " << endl;
-        cout << "\n[L]  Logowanie;\n[R]  Rejestracja;\n[Z]  Zmiana hasla;\n[W]  Wyszukaj film;\n[K]  Kategorie filmów;\n[P]  Proponowane filmy;\n[U]  Lista życzeń;\n[D]  Dane kontaktowe;" << endl;
-        char wybor;
-        SetConsoleTextAttribute(kolor, 8);
-        cout << "\n\nTwój wybór: ";
-        SetConsoleTextAttribute(kolor, 15);
-        cin >> wybor;
-        if (stan_uz == 1) { //Zalogowany uzytkownik
-            switch (toupper(wybor)) {
-                SetConsoleTextAttribute(kolor, 15);
-
-                //===================UWAGA! Funckie nie przyjmują zmiennych!===================//
-
-            case 'L':
-                cout << "Jesteś już zalogowanym użytkownikiem" << endl;
-                SetConsoleTextAttribute(kolor, 8);
-                system("pause");
-                system("cls");
-                stan = false;
-                break;
-            case 'R':
-                cout << "Jesteś zarejestrowanym użytkownikiem" << endl;
-                SetConsoleTextAttribute(kolor, 8);
-                system("pause");
-                system("cls");
-                stan = false;
-                break;
-            case 'Z':
+			case 'L':
+				cout << "Jesteś już zalogowanym użytkownikiem" << endl;
+				SetConsoleTextAttribute(kolor, 8);
+				system("pause");
+				system("cls");
+				stan = false;
+				break;
+			case 'R':
+				cout << "Jesteś zarejestrowanym użytkownikiem" << endl;
+				SetConsoleTextAttribute(kolor, 8);
+				system("pause");
+				system("cls");
+				stan = false;
+				break;
+			case 'Z':
                 Klient.Zmiana_hasla();
-                stan = false;
-                break;
-            case 'W':
-                //				wyszukaj(); 			
-                break;
-            case 'K':
-                //				wyszukaj(); 
-                break;
-            case 'P':
-                //				proponowane(stan_uz);
-                break;
-            case 'U':
-                //				lista_zyczen();  <- Do wyrzucenia?
-                break;
-            case 'D':
+				break;
+			case 'W':
+				//				wyszukaj(); 			
+				break;
+			case 'K':
+				//				wyszukaj(); 
+				break;
+			case 'P':
+				//				proponowane(stan_uz);
+				break;
+			case 'U':
+				//				lista_zyczen();  <- Do wyrzucenia?
+				break;
+			case 'D':
                 system("cls");
                 Dane_kontaktowe.wyswietl_kontakt();
                 system("pause");
                 stan = false;
-                break;
-            default:
-                system("cls");
-                SetConsoleTextAttribute(kolor, 10);
-                cout << "Wybrono niepoprawna opcje, prosze sprobowac ponownie\n\n";
-                SetConsoleTextAttribute(kolor, 8);
-                system("pause");
-                system("cls");
-                stan = false;
-                break;
-            }
+				break;
+			default:
+				system("cls");
+				SetConsoleTextAttribute(kolor, 10);
+				cout << "Wybrono niepoprawna opcje, prosze sprobowac ponownie\n\n";
+				SetConsoleTextAttribute(kolor, 8);
+				system("pause");
+				system("cls");
+				stan = false;
+				break;
+			}
 
 
-        }
-        else if (stan_uz == 0) { //Uzytkownik nie jest zalogowany
-            switch (toupper(wybor)) {
-            case 'L':
+		}
+		else if (stan_uz == 0) { //Uzytkownik nie jest zalogowany
+			switch (wybor) {
+			case 'L':
                 system("cls");
-                stan_uz = Klient.Logowanie();
-                SetConsoleTextAttribute(kolor, 8);
-                system("cls");
+                Klient.Logowanie();
                 system("pause");
                 stan = false;
                 break;
-            case 'R':
+			case 'R':
                 system("cls");
                 Klient.Rejestracja(listaloginow);
                 system("pause");
                 stan = false;
                 break;
-            case 'Z':
+			case 'Z':
                 cout << "Odmowa dostępu. Usługa dostępna wyłącznie dla zalogowanych użytkowników" << endl;
-                system("pause");
                 stan = false;
                 break;
-            case 'W':
-                //				wyszukaj();
-                break;
-            case 'K':
-                //				wyszukaj();
-                break;
-            case 'P':
-                //				proponowane(stan_uz);
-                break;
-            case 'U':
-                cout << "Odmowa dostępu. Usługa dostępna wyłącznie dla zalogowanych użytkowników" << endl;
-                system("pause");
-                stan = false;
-                break;
-            case 'D':
+			case 'W':
+				//				wyszukaj();
+				break;
+			case 'K':
+				//				wyszukaj();
+				break;
+			case 'P':
+				//				proponowane(stan_uz);
+				break;
+			case 'U':
+				cout << "Odmowa dostępu. Usługa dostępna wyłącznie dla zalogowanych użytkowników" << endl;
+				stan = false;
+				break;
+			case 8:
                 system("cls");
                 Dane_kontaktowe.wyswietl_kontakt();
-                system("pause");
-                stan = false;
-                break;
-            default:
-                system("cls");
-                SetConsoleTextAttribute(kolor, 10);
-                cout << "Wybrono niepoprawna opcje, prosze sprobowac ponownie\n\n";
-                SetConsoleTextAttribute(kolor, 8);
-                system("pause");
-                system("cls");
-                stan = false;
-                break;
-            }
-        }
-    } while (stan == false);
+				break;
+			default:
+				break;
+			}
+		}
+	} while (stan == false);
 }
 
 int main() {
-    setlocale(LC_ALL, "polish");
-    kolor = GetStdHandle(STD_OUTPUT_HANDLE);
-    menu(stan_uz);
+	setlocale(LC_ALL, "polish");
+	kolor = GetStdHandle(STD_OUTPUT_HANDLE);
+	menu(stan_uz);
 
 }
